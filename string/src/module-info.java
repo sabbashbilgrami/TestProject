3eb5ee413939487c5a34035bda59f952b1e4c7a6
sabbashbilgrami <52467005+sabbashbilgrami@users.noreply.github.com>
2022-11-22 12:57:34 +0530

    new commit

---
+++ string/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author abbas
+ *
+ */
+module string {
+}
---
+++ string/src/strings/String.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		System.out.println("selenium");
+		
+		System.out.println("selenium".concat("java"));
+
+	}
+
+}
